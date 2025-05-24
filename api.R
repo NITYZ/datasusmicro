b19e42415c163b8cdbc6c8144d8e74bcc2aed7a9
NITYZ <luisgustavomf27@gmail.com>
2025-05-24 17:58:55 -0300

    Create api.R

---
+++ api.R
@@ -0,0 +1,17 @@
+# plumber/api.R
+library(plumber)
+library(microdatasus)
+
+#* @get /fetch_data
+function(year_start, year_end, uf, information_system) {
+  dados <- fetch_datasus(as.integer(year_start), as.integer(year_end), uf, information_system)
+  
+  dados_proc <- switch(information_system,
+    "SIM-DO" = process_sim(dados),
+    "SINASC" = process_sinasc(dados),
+    "SIH-RD" = process_sih(dados),
+    stop("Sistema inválido.")
+  )
+  
+  return(dados_proc)
+}
